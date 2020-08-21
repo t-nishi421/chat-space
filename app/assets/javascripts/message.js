ba8398053bc9d65d9947e890444ee507e0650345
t-nishi421 <69189773+t-nishi421@users.noreply.github.com>
2020-08-21 14:25:46 +0900

    送信したメッセージをhtmlに表示まで

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,46 @@
 $(function(){
+  function buildHTML(message) {
+    let html = ""
+    if (message.image) {
+      html = 
+        `<div class="message-items">
+          <div class="message-items__top">
+            <div class="message-items__top__name">
+              ${message.user_name}
+            </div>
+            <div class="message-items__top__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="message-items__message">
+            <p>
+              ${message.content}
+            </p>
+            <img class="Message-items__message__image" src="${message.image}">
+          </div>
+        </div>`
+    } else {
+      html = 
+        `<div class="message-items">
+          <div class="message-items__top">
+            <div class="message-items__top__name">
+              ${message.user_name}
+            </div>
+            <div class="message-items__top__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="message-items__message">
+            <p>
+              ${message.content}
+            </p>
+          </div>
+        </div>`
+    }
+
+    return html;
+  }
+
   $("#new-content").on('submit', function(e){
     e.preventDefault()
     let formData = new FormData(this)
@@ -11,6 +53,11 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
+      $('.MessageField').append(html);
+      $('form')[0].reset();
     })
   });
 });
