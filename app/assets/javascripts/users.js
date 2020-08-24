f4cffabcbc7e07423e81b99a3224d820dde125d8
t-nishi421 <69189773+t-nishi421@users.noreply.github.com>
2020-08-24 11:15:46 +0900

    hamlに描画まで実装

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,4 +1,22 @@
 $(function(){
+  function addUser(user){
+    let html = `
+                <div class="ChatMember clearfix">
+                  <p class="ChatMember__name">${user.name}</p>
+                  <div class="ChatMember__add ChatMember__button" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+                </div>
+               `;
+    $("#UserSearchResult").append(html);
+  }
+
+  function noUser(){
+    let html = `
+                <div class="ChatMember clearfix">
+                  <p class="ChatMember__name">ユーザーが見つかりません</p>
+                </div>
+               `;
+    $("#UserSearchResult").append(html);
+  }
 
   $("#UserSearch__field").on("keyup", function(){
     let input = $("#UserSearch__field").val();
@@ -9,10 +27,19 @@ $(function(){
       dataType: "json"
     })
     .done(function(users){
-      console.log(users);
+      $("#UserSearchResult").empty();
+      if (users.length !== 0) {
+        users.forEach(function(user){
+          addUser(user);
+        });
+      } else if (input.length == 0) {
+        return false;
+      } else {
+        noUser();
+      }
     })
-    .fail(function(users){
-      console.log(users + " sippai")
-    })
+    .fail(function(){
+      alert("ユーザー検索に失敗しました")
+    });
   });
 });
